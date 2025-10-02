61d97028e195599780e91d2c3619fa33928ea9bc
UIT-BaoTuong <23521750@gm.uit.edu.vn>
2025-10-02 23:21:55 +0700

    Create project

---
+++ Ecowalk/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RestController
+	public class HelloController {
+		@GetMapping("/")
+		public String hello() {
+			return "Hello from nho hahasss alo m 7!";
+		}
+	}
+}
