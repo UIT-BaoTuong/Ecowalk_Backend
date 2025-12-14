8d01a9001b2efd979b5c6ab1fe8c5b53a41004cc
BaoTuong <tuongndb>
2025-12-14 23:13:18 +0700

    Test version 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
 	public class HelloController {
 		@GetMapping("/")
 		public String hello() {
-			return "Hello from nho helo hi 7, Add git hub action test!";
+			return "Version v2";
 		}
 		@GetMapping("/test")
 		public String test() {
